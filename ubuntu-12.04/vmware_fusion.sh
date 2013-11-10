778362cfca5d7ebd8d550bf01f69f3f8ed6648f7
Alexander Saltanov <asd@mokote.com>
2013-11-10 12:23:46 +0400

    Avoid implicit path to VMWare Tools.

--- ubuntu-12.04/vmware_fusion.sh
+++ ubuntu-12.04/vmware_fusion.sh
@@ -6,7 +6,7 @@ apt-get -y install gcc build-essential linux-headers-$(uname -r)
 # Install the VMware Fusion guest tools
 cd /tmp
 mkdir -p /mnt/cdrom
-mount -o loop ~/linux.iso /mnt/cdrom
+mount -o loop /home/vagrant/linux.iso /mnt/cdrom
 tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
 /tmp/vmware-tools-distrib/vmware-install.pl -d
 rm /home/vagrant/linux.iso
