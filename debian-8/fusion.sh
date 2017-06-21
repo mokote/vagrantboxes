450512fa2ff8c5ed8ddfa85d25ce7bc81902529b
Alexander Saltanov <asd@mokote.com>
2017-06-22 00:42:31 +0300

    Drop old stable.

--- debian-8/fusion.sh
+++
@@ -1,1 +0,0 @@
-echo "We don't build for VMWare Fusion any more. Feel free to send a push request for this."
--- debian-8/vagrant_keys.sh
+++
@@ -1,8 +0,0 @@
-apt-get install -y ca-certificates # Needed for authorized_keys to work
-
-# Installing vagrant keys
-mkdir /home/vagrant/.ssh
-wget --no-check-certificate 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
-chmod 600 /home/vagrant/.ssh/authorized_keys
-chmod 700 /home/vagrant/.ssh
-chown -R vagrant /home/vagrant/.ssh
